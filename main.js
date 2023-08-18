63081f20f64763be25edccde737c071ec7a9d8b4
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-08-18 23:19:51 +0600

    mega menu start

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 import "./styles.scss";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuItems = document.querySelectorAll(".menu-item");
+
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      const submenu = item.querySelector(".submenu");
+      if (submenu) {
+        submenu.style.display = "block";
+      }
+    });
+
+    item.addEventListener("mouseleave", () => {
+      const submenu = item.querySelector(".submenu");
+      if (submenu) {
+        submenu.style.display = "none";
+      }
+    });
+  });
+});
